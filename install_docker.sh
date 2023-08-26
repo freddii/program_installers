19b58d675a9e75dfe6d9322b3324720050ed4679
freddii <freddii@users.noreply.github.com>
2023-08-26 13:02:11 +0200

    update

--- install_docker.sh
+++ install_docker.sh
@@ -7,29 +7,36 @@ set -eu
 #based on https://docs.docker.com/engine/install/ubuntu/
 #grep --color --perl-regexp 'vmx|svm' /proc/cpuinfo
 
-sudo apt-get update
+sudo apt -y update && sudo apt -y upgrade
 #setup the repo
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
+#sudo apt-get install \
+#    apt-transport-https \
+#    ca-certificates \
+#    curl \
+#    gnupg-agent \
+#    software-properties-common -y
+sudo apt install lsb-release gnupg2 apt-transport-https ca-certificates curl software-properties-common -y
 #Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/debian.gpg
 #verify key
-sudo apt-key fingerprint 0EBFCD88 
+#sudo apt-key fingerprint 0EBFCD88 
 #search for 9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88
 #add stable amd64 repo
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+#sudo add-apt-repository \
+#   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) \
+#   stable"
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 #update repo and install docker
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 #test docker
 sudo docker run hello-world
+
+echo "add your user to docker group to run docker commands"
+echo "sudo usermod -aG docker $USER"
+#
 #
 ##check what containers are running
 #sudo docker ps
