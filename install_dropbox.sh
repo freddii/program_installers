daed0f3363f1df773a8443462b2df298308357bc
freddii <freddii@users.noreply.github.com>
2023-08-27 18:12:02 +0100

    update

--- install_dropbox.sh
+++ install_dropbox.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #to run this script go into the folder where it is then run: ./install_dropbox.sh
-cd /home/$USER
+cd /tmp
 #wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
-wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2022.12.05_amd64.deb
+#wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2022.12.05_amd64.deb
+wget https://linux.dropbox.com/packages/ubuntu/dropbox_2022.12.05_amd64.deb
 #/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
 #wget -q --show-progress -O dropbox.deb https://www.dropbox.com/download?$(wget -q https://www.dropbox.com/de/install-linux -O - | egrep 'dl=packages/ubuntu/dropbox_[0-9,.-]{1,20}_amd64.deb' -o | head -1)
 sudo dpkg -i dropbox*.deb
