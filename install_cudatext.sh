6aefd2f45995411804f8c9e35fd37f0e059afeb2
freddii <freddii@users.noreply.github.com>
2023-08-27 12:10:04 +0100

    update

--- install_cudatext.sh
+++ install_cudatext.sh
@@ -6,12 +6,13 @@ set -eu
 #cd /home/$USER
 #wget https://sourceforge.net/projects/cudatext/files/release/Linux/cudatext_1.63.0.0-1_gtk2_amd64.deb/download
 #wget https://netcologne.dl.sourceforge.net/project/cudatext/release/Linux/cudatext_1.61.0.0-1_gtk2_amd64.deb
+wget -O cudatext_amd64.deb https://master.dl.sourceforge.net/project/cudatext/release/1.197.0.2/cudatext_1.197.0.2-1_gtk2_amd64.deb
 #wget -O cudatext_amd64.deb $(wget https://sourceforge.net/projects/cudatext/rss?path=/release/Linux -O - | egrep 'https:/.*/.*/cudatext_.*gtk2_amd64.deb/download' -o | head -1)
 #wget -O cudatext_amd64.deb $(wget https://sourceforge.net/p/cudatext/activity/feed -O - | egrep 'https:/.*/.*/cudatext_.*gtk2_amd64.deb/download' -o | head -1)
 #wget -O cudatext_amd64.deb $(wget https://www.fosshub.com/CudaText.html  -O - | egrep 'https://www.fosshub.com/CudaText.html\?dwl=cudatext_[0-9,_.-]{1,20}_gtk2_amd64.deb' -o | head -1)
-#sudo dpkg -i cudatext_amd64.deb
-#sudo apt-get install -f -y
-#rm cudatext_amd64.deb
+sudo dpkg -i cudatext_amd64.deb
+sudo apt-get install -f -y
+rm cudatext_amd64.deb
 #
 #
 folder_name="cudatext"
@@ -28,14 +29,15 @@ folder_name="cudatext"
 #git clone https://github.com/Alexey-T/CudaText_up
 #cd CudaText_up
 #./cudaup.sh -g -m
-echo "simply download the deb file from:
-https://www.fosshub.com/CudaText.html
-https://sourceforge.net/projects/cudatext/files/release/
-Then install it with:
-cd $HOME/Downloads
-sudo dpkg -i cudatext_*.deb
 
-plugins > addon manager > install from github:
+#echo "simply download the deb file from:
+#https://www.fosshub.com/CudaText.html
+#https://sourceforge.net/projects/cudatext/files/release/
+#Then install it with:
+#cd $HOME/Downloads
+#sudo dpkg -i cudatext_*.deb"
+
+echo "plugins > addon manager > install from github:
 https://github.com/OlehL/cuda_differ
 https://github.com/CudaText-addons/cuda_terminal
 https://github.com/OlehL/cuda_plain_tasks
