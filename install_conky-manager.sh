c26d690c1a8198389d116c6f43a24d0a063d0e0e
freddii <freddii@users.noreply.github.com>
2023-08-27 15:19:28 +0100

    update

--- install_conky-manager.sh
+++ install_conky-manager.sh
@@ -9,7 +9,7 @@ if [ -d "/home/$USER/programs_setup/conky-manager2" ]; then
   rm -rf /home/$USER/programs_setup/conky-manager2
 fi
 dpkg -l | grep -qw conky-all || sudo apt-get install conky-all -y
-sudo apt-get install build-essential git valac libgee-0.8-dev libgtk-3-dev libjson-glib-dev gettext -y
+sudo apt-get install build-essential git valac libgee-0.8-dev libgtk-3-dev libjson-glib-dev gettext make -y
 cd /home/$USER/
 mkdir -p /home/$USER/programs_setup/
 cd /home/$USER/programs_setup/
@@ -18,9 +18,11 @@ cd conky-manager2
 make
 #sudo checkinstall #not working
 #./build-deb.sh #sudo apt install bzr-builddeb bzr
+sudo apt install rsync psmisc -y
+#
 sudo make install
 mkdir -p /home/$USER/.conky/
 cd /home/$USER/.conky/
 rm -rf /home/$USER/.conky/conky
 git clone https://github.com/freddii/conky
-echo "now put your conky into /home/$USER/.conky/"
+echo "i have put some conkys into /home/$USER/.conky/"
