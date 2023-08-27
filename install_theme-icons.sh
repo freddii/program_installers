e607bb795885d77ea897663835b3a60553f63fe9
freddii <freddii@users.noreply.github.com>
2023-08-27 13:06:52 +0100

    update

--- install_theme-icons.sh
+++ install_theme-icons.sh
@@ -4,5 +4,5 @@
 #set -eu
 #
 sudo apt install arc-theme papirus-icon-theme -y #numix-icon-theme
-sudo apt install elementary-xfce-icon-theme -y
+#sudo apt install elementary-xfce-icon-theme -y
 echo "Settings manager > Appearance > Style >Arc-dark / Icons > Papirus-Dark"
