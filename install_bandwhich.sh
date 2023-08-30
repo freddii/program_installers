c0fbbb113ba77f741d121d4638150d838a49c18e
freddii <freddii@users.noreply.github.com>
2023-08-30 10:18:05 +0100

    update

--- install_bandwhich.sh
+++ install_bandwhich.sh
@@ -11,7 +11,8 @@ check_install unp
 
 cd /tmp
 #wget https://github.com/imsnif/bandwhich/releases/download/0.16.0/bandwhich-v0.16.0-x86_64-unknown-linux-musl.tar.gz
-wget -q --show-progress https://github.com$(wget -q https://github.com/imsnif/bandwhich/releases/latest -O - | egrep '/.*/.*/bandwhich-v[0-9,_.-]{1,20}-x86_64-unknown-linux-musl.tar.gz' -o)
+wget https://github.com/imsnif/bandwhich/releases/download/0.20.0/bandwhich-v0.20.0-x86_64-unknown-linux-musl.tar.gz
+#wget -q --show-progress https://github.com$(wget -q https://github.com/imsnif/bandwhich/releases/latest -O - | egrep '/.*/.*/bandwhich-v[0-9,_.-]{1,20}-x86_64-unknown-linux-musl.tar.gz' -o)
 unp bandwhich-*-x86_64-unknown-linux-musl.tar.gz
 #cd bandwhich-v0.16.0-x86_64-unknown-linux-musl
 #
