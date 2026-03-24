8315649d2db4ea5df5628f83c6a59d75b2a0b0cc
freddii <freddii@users.noreply.github.com>
2026-03-24 15:55:41 +0000

    virtualenv for thonny with matplotlib

--- install_thonny.sh
+++ install_thonny.sh
@@ -2,6 +2,16 @@
 #to run this script go into the folder where it is then run: ./install_thonny.sh
 #
 sudo apt install thonny -y
+cd $HOME
+mkdir powerful_python
+cd powerful_python
+python3 -m venv my_venv
+source my_venv/bin/activate
+pip install matplotlib
+deactivate
+echo "click on the three dots at the bottom right and choose 'Configure interpreter'
+then right to 'python executeable' on the three dots and choose
+$HOME/powerful_python/my_venv/bin/python3"
 exit
 #
 cd /tmp
